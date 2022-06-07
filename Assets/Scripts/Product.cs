f29e594eeda3ab83915b7b19bf9eb1bdb8b493f6
Muirehe <pya.alisa@ro.ru>
2022-06-07 21:49:53 +0300

    fixed product

--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -1,11 +1,20 @@
+using UnityEngine;
+
 namespace miniit.MERGE
 {
     public class Product : ColoringStoringObject
     {
+        [SerializeField] private StoringObject storingObject;
+
         public override void ColorImage()
         {
             image.sprite = StoringObjectInfo.ProductImage;
             image.color = StoringObjectInfo.Color;
+        }
+
+        private void OnDestroy()
+        {
+            storingObject.FreePlace();
         }
     }
 }
